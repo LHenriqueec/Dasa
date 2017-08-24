9b5ecc29acca8ac14ffdc3e3fd42f3d14823dc7c
Luiz Henrique <luizhenrique.ecl@gmail.com>
2017-08-24 14:11:12 -0300

    Corrigido a Json de entidades com atributos bidirecionais

---
+++ src/br/com/iveso/dasa/util/EstrategiaExclusaoJSON.java
@@ -0,0 +1,20 @@
+package br.com.iveso.dasa.util;
+
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
+
+import br.com.iveso.dasa.annotaion.ExcluirJSON;
+
+public class EstrategiaExclusaoJSON implements ExclusionStrategy {
+
+	@Override
+	public boolean shouldSkipClass(Class<?> type) {
+		return false;
+	}
+
+	@Override
+	public boolean shouldSkipField(FieldAttributes fa) {
+		return fa.getAnnotation(ExcluirJSON.class) != null;
+	}
+
+}
